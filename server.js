a84499a055383250d79a474a458257ff7ce99ff4
Jaime D <134320299+jdoerr13@users.noreply.github.com>
2025-04-12 10:31:54 -0400

    Temporarily sync User model to fix missing firstname column

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const app = require('./app');
 const { PORT } = require('./config');
+const { syncUserModel } = require('./models/user'); // ← Add this line
+
+syncUserModel(); // ← Add this line
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
